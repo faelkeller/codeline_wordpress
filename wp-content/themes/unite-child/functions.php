4c5bb2f021086d470b70d41f44f49d5a87e6bb87
Rafael Keller <faelkeller@gmail.com>
2017-11-20 10:39:00 -0200

    add new type of post

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -4,3 +4,31 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+add_action('init', 'type_post_films');
+ 
+    function type_post_films() { 
+        $labels = array(
+            'name' => _x('Films', 'post type general name'),
+            'singular_name' => _x('Film', 'post type singular name'),            
+            'parent_item_colon' => '',
+            'menu_name' => 'Films'
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'public_queryable' => true,
+            'show_ui' => true,           
+            'query_var' => true,
+            'rewrite' => true,
+            'capability_type' => 'post',
+            'has_archive' => true,
+            'hierarchical' => false,
+            'menu_position' => null,                  
+            'supports' => array('title','editor','thumbnail','comments', 'excerpt', 'custom-fields', 'revisions', 'trackbacks')
+        );
+ 
+        register_post_type( 'films' , $args );
+        flush_rewrite_rules();
+}
