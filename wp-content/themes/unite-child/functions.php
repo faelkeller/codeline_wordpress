2364a010faac7015cfecf1e63f41498b50ac0d2b
Rafael Keller <faelkeller@gmail.com>
2017-11-20 11:12:42 -0200

    add custom taxonomies

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -1,34 +1,87 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+
 function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
 add_action('init', 'type_post_films');
- 
-    function type_post_films() { 
-        $labels = array(
-            'name' => _x('Films', 'post type general name'),
-            'singular_name' => _x('Film', 'post type singular name'),            
-            'parent_item_colon' => '',
-            'menu_name' => 'Films'
-        );
 
-        $args = array(
-            'labels' => $labels,
+function type_post_films() {
+    $labels = array(
+        'name' => _x('Films', 'post type general name'),
+        'singular_name' => _x('Film', 'post type singular name'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Films'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'public_queryable' => true,
+        'show_ui' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array('title', 'editor', 'thumbnail', 'comments', 'excerpt', 'custom-fields', 'revisions', 'trackbacks')
+    );
+
+    register_post_type('films', $args);
+    flush_rewrite_rules();
+}
+
+add_action('init', 'create_film_tax');
+
+function create_film_tax() {
+    register_taxonomy(
+        "genres", 
+        "films", 
+        array(
+            "label" => "Genres",
+            "singular_label" => "Genre",
             'public' => true,
-            'public_queryable' => true,
-            'show_ui' => true,           
-            'query_var' => true,
             'rewrite' => true,
-            'capability_type' => 'post',
-            'has_archive' => true,
-            'hierarchical' => false,
-            'menu_position' => null,                  
-            'supports' => array('title','editor','thumbnail','comments', 'excerpt', 'custom-fields', 'revisions', 'trackbacks')
-        );
- 
-        register_post_type( 'films' , $args );
-        flush_rewrite_rules();
+            'hierarchical' => true,
+        )
+    );
+    
+    register_taxonomy(
+        "country", 
+        "films", 
+        array(
+            "label" => "Countries",
+            "singular_label" => "Country",
+            'public' => true,
+            'rewrite' => true,
+            'hierarchical' => true,
+        )
+    );
+    
+    register_taxonomy(
+        "year", 
+        "films", 
+        array(
+            "label" => "Years",
+            "singular_label" => "Year",
+            'public' => true,
+            'rewrite' => true,
+            'hierarchical' => true,
+        )
+    );
+    
+    register_taxonomy(
+        "actor", 
+        "films", 
+        array(
+            "label" => "Actors",
+            "singular_label" => "Actor",
+            'public' => true,
+            'rewrite' => true,
+            'hierarchical' => true,
+        )
+    );
 }
